48db17aecc4e42852d8d1acc53a9417a72d36242
genrich <9enrich@gmail.com>
2013-06-15 09:19:07 +0300

    improved dynamic object generation

--- www/js/Viewport.js
+++ www/js/Viewport.js
@@ -1,34 +1,64 @@
+var CONST =
+{
+    MsgType
+    {
+        Stimulus: 0
+    }
+}
+
 function Viewport (sig)
 {
     var viewport = new UI.Panel ({clazz: "Viewport"})
 
     if (!Detector.webgl) Detector.addGetWebGLMessage ()
 
-    var stimulus = new THREE.Geometry ()
-    for (var i = 0; i < 800; i++)
+    var sprite = THREE.ImageUtils.loadTexture ("textures/ball.png")
+    var stimulusMaterial = new THREE.ParticleBasicMaterial ({size: 20, map: sprite, vertexColors: true, alphaTest: 0.5})
+
+    var stimulus = new THREE.ParticleSystem (new THREE.Geometry (), stimulusMaterial)
+
+    var cloneFill = function (array, value, count)
     {
-        stimulus.vertices[i] = new THREE.Vector3 ()
-        stimulus.colors[i] = new THREE.Color ()
+        var new_array = []
+        for (var i = 0; i < count; i++)
+            if (array[i]) new_array[i] = array[i].clone ()
+            else          new_array[i] = value.clone ()
+        return new_array
     }
+    var reserve = function (id) { return Math.ceil ((id + 1) / 100) * 100 }
+
     var geometry = new THREE.Geometry ()
     var geometry2 = new THREE.Geometry ()
     var colors = []
     var count = 0
+
     sig.wsMsgReceived.add (function (buffer)
     {
         var type = new Uint32Array (buffer, 0, 1) [0]
 
-        if (type == 0) // Stimulus
+        if (type == CONST.MsgType.Stimulus)
         {
             var id = new Uint32Array (buffer, 4, 1) [0]
-            var x = new Float32Array (buffer,  8, 1) [0]
+            var x = new Float32Array (buffer, 8,  1) [0]
             var y = new Float32Array (buffer, 12, 1) [0]
             var z = new Float32Array (buffer, 16, 1) [0]
 
-            stimulus.vertices[id] = new THREE.Vector3 (x, y, z)
-            stimulus.colors[id] = new THREE.Color (0xffffff)
-            stimulus.verticesNeedUpdate = true
-            stimulus.colorsNeedUpdate = true
+            var geom = stimulus.geometry
+            if (geom.vertices.length <= id)
+            {
+                geom = new THREE.Geometry ()
+                geom.vertices = cloneFill (stimulus.geometry.vertices, new THREE.Vector3 (0, 0, 10000), reserve (id))
+                geom.colors   = cloneFill (stimulus.geometry.colors,   new THREE.Color (),              reserve (id))
+
+                scene.remove (stimulus)
+                stimulus = new THREE.ParticleSystem (geom, stimulusMaterial)
+                scene.add (stimulus)
+            }
+
+            geom.vertices[id] = new THREE.Vector3 (x, y, z)
+            geom.colors[id] = new THREE.Color (0xffffff)
+            geom.verticesNeedUpdate = true
+            geom.colorsNeedUpdate   = true
         }
     })
 
@@ -43,17 +73,10 @@ function Viewport (sig)
     var scene = new THREE.Scene ()
     scene.fog = new THREE.FogExp2 (0x000000, 0.001)
 
-    scene.add (function () // Stimulus
-    {
-        var sprite = THREE.ImageUtils.loadTexture ("textures/ball.png")
-        var material = new THREE.ParticleBasicMaterial ({size: 20, map: sprite, vertexColors: true, alphaTest: 0.5})
-        return new THREE.ParticleSystem (stimulus, material)
-    } ())
+    scene.add (stimulus)
 
     scene.add (function ()
     {
-        var sprite = THREE.ImageUtils.loadTexture ("textures/ball.png")
-
         for (var i = 0; i < 100; i++)
         {
             var vertex = new THREE.Vector3 (Math.random () * 380 - 190, Math.random () * 580 - 290, Math.random () * 380 - 190)
